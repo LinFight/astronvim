bf854db1be0d4fbfb7348b574f2655d5cf609482
LinFight <1095722147@qq.com>
2024-08-20 14:42:41 +0800

    add color theme

--- lua/community.lua
+++ lua/community.lua
@@ -5,7 +5,15 @@
 ---@type LazySpec
 return {
   "AstroNvim/astrocommunity",
+  -- colorscheme
+  { import = "astrocommunity.colorscheme.rose-pine" },
+  { import = "astrocommunity.colorscheme.bamboo-nvim" },
+  { import = "astrocommunity.colorscheme.miasma-nvim" },
+  { import = "astrocommunity.colorscheme.melange-nvim" },
+  { import = "astrocommunity.colorscheme.everforest" },
+  -- lang
   { import = "astrocommunity.pack.lua" },
   { import = "astrocommunity.pack.python" },
+  { import = "astrocommunity.pack.elixir-phoenix" },
   -- import/override with your plugins folder
 }
